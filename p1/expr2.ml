8ecd92a2f955e5b7524a5026c943b1d76466ebb6
Rodrigo Naranjo González <56093078+rng1@users.noreply.github.com>
2021-10-08 08:58:08 +0200

    initial commit with practicals 1 and 2

---
+++ p1/expr2.ml
@@ -0,0 +1,5 @@
+let u = 2 * 2 + 6 / 3;;
+let v = 2. * asin 1.;;
+(* let w = *)
+let x = (2 * 2 > 0) && (5-3 != 2);;
+let y = if 1 > 0 then "correcto" else "error";;
---
+++ p1/nombre.ml
@@ -0,0 +1,2 @@
+print_string "Rodrigo Naranjo González\n"
+print_string "r.naranjo@udc.es\n"
